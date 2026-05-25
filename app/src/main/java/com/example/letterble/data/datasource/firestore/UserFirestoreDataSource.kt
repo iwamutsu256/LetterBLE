a896b88f715682639872add1f3e51ac4baecc52a
tkhy2651 <tah331925@gmail.com>
2026-05-25 17:38:58 +0900

    UseFirestoreDataSource

--- app/src/main/java/com/example/letterble/data/datasource/firestore/UserFirestoreDataSource.kt
+++ app/src/main/java/com/example/letterble/data/datasource/firestore/UserFirestoreDataSource.kt
@@ -18,3 +18,64 @@
 // TODO: queryでfilterする
 // TODO: documentをdataクラスに変換する
 // TODO: timestampを適切な型に変換する
+
+package com.example.letterble.data.datasource.firestore
+
+import com.example.letterble.domain.model.User
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+class UserFirestoreDataSource(
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+) {
+    private val usersCollection = firestore.collection(FirestoreCollections.USERS)
+
+    suspend fun saveUser(user: User) {
+        val data = mapOf(
+            FirestoreFields.User.USER_NAME to user.userName,
+            FirestoreFields.User.CARRYING_LETTER_IDS to user.carryingLetterIds
+        )
+
+        usersCollection
+            .document(user.userName)
+            .set(data)
+            .awaitResult()
+    }
+
+    suspend fun getUser(userName: String): User? {
+        val document = usersCollection
+            .document(userName)
+            .get()
+            .awaitResult()
+
+        if (!document.exists()) {
+            return null
+        }
+
+        val carryingLetterIds = document
+            .get(FirestoreFields.User.CARRYING_LETTER_IDS)
+            as? List<*>
+
+        return User(
+            userName = document.getString(FirestoreFields.User.USER_NAME).orEmpty(),
+            carryingLetterIds = carryingLetterIds
+                ?.filterIsInstance<String>()
+                ?: emptyList()
+        )
+    }
+}
+
+private suspend fun <T> Task<T>.awaitResult(): T {
+    return suspendCancellableCoroutine { continuation ->
+        addOnSuccessListener { result ->
+            continuation.resume(result)
+        }
+
+        addOnFailureListener { exception ->
+            continuation.resumeWithException(exception)
+        }
+    }
+}
