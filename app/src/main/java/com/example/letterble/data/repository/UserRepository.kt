751fcccf23f07a96e766dfeac17c0fd53c91e826
tkhy2651 <tah331925@gmail.com>
2026-05-25 17:42:42 +0900

    UserRepository.kt

--- app/src/main/java/com/example/letterble/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/letterble/data/repository/UserRepository.kt
@@ -12,3 +12,24 @@
 // TODO: DataSourceの関数を呼び出すラッパーを作る
 // TODO: 上位層に返すデータの整形を行う（必要なら）
 // TODO: ビジネスロジックを書かない
+package com.example.letterble.data.repository
+
+import com.example.letterble.data.datasource.firestore.UserFirestoreDataSource
+import com.example.letterble.domain.model.User
+
+class UserRepository(
+    private val userFirestoreDataSource: UserFirestoreDataSource = UserFirestoreDataSource()
+) {
+    suspend fun saveUser(userName: String) {
+        val user = User(
+            userName = userName,
+            carryingLetterIds = emptyList()
+        )
+
+        userFirestoreDataSource.saveUser(user)
+    }
+
+    suspend fun getUser(userName: String): User? {
+        return userFirestoreDataSource.getUser(userName)
+    }
+}
